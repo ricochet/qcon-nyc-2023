a31d74a45f3748750ae4ee2b0611a34bbb6717fb
Bailey Hayes <behayes2@gmail.com>
2023-06-27 15:34:38 -0400

    fix: yeet http

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,18 +1,8 @@
-use anyhow::Result;
-
 use wasmtime::{
     component::{Component, Linker},
     Config, Engine, Store,
 };
 use wasmtime_wasi::preview2::{self, Table, WasiCtx, WasiCtxBuilder, WasiView};
-// use wasmtime_wasi_http::WasiHttp;
-
-use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
-use hyper::body::Frame;
-use hyper::server::conn::http1;
-use hyper::{body::Bytes, service::service_fn, Method, Request, Response, StatusCode};
-use std::{net::SocketAddr};
-use tokio::net::TcpListener;
 
 struct ReactorCtx {
     table: Table,
@@ -39,42 +29,6 @@ impl WasiView for ReactorCtx {
 async fn main() -> wasmtime::Result<()> {
     println!("Hello, from the Rust Host!");
 
-    let component = r#"
-    (component
-      (type $t (func (param "a" u32) (result u32)))
-      (component $a
-        (core module $m
-          (func (export "add-five") (param i32) (result i32)
-            local.get 0
-            i32.const 5
-            i32.add)
-        )
-        (core instance $m (instantiate $m))
-        (func (export "add-five") (type $t) (canon lift (core func $m "add-five")))
-      )
-      (component $b
-        (import "interface-v1" (instance $i
-          (export "add-five" (func (type $t)))))
-        (core module $m
-          (func $add-five (import "interface-0.1.0" "add-five") (param i32) (result i32))
-          (func) ;; causes index out of bounds
-          (func (export "run") (result i32) i32.const 0 call $add-five)
-        )
-        (core func $add-five (canon lower (func $i "add-five")))
-        (core instance $i (instantiate 0
-          (with "interface-0.1.0" (instance
-            (export "add-five" (func $add-five))
-          ))
-        ))
-        (func (result u32) (canon lift (core func $i "run")))
-        (export "run" (func 1))
-      )
-      (instance $a (instantiate $a))
-      (instance $b (instantiate $b (with "interface-v1" (instance $a))))
-      (export "run" (func $b "run"))
-    )
-    "#;
-
     let mut config = Config::new();
     config.wasm_backtrace_details(wasmtime::WasmBacktraceDetails::Enable);
     config.wasm_component_model(true);
@@ -83,8 +37,7 @@ async fn main() -> wasmtime::Result<()> {
     // An engine stores and configures global compilation settings like
     // optimization level, enabled wasm features, etc.
     let engine = Engine::new(&config).unwrap();
-    let component = Component::from_file(&engine, "/Users/bhayes/repos/ricochet/qcon-nyc-2023/guest/linked.wasm")?;
-    // let component = Component::new(&engine, component)?;
+    let component = Component::from_file(&engine, "../guest/linked.wasm")?;
 
     // A `Store` is what will own instances, functions, globals, etc. All wasm
     // items are stored within a `Store`, and it's what we'll always be using to
@@ -107,93 +60,14 @@ async fn main() -> wasmtime::Result<()> {
 
     let instance = linker.instantiate_async(&mut store, &component).await?;
 
-
-
     // let run = instance.get_typed_func::<(), (u32,)>(&mut store, "run")?;
     // let ret = run.call_async(&mut store, ()).await?;
     // println!("result of run:\n{}", ret.0);
-
-
 
     let run = instance.get_typed_func::<(), (String,)>(&mut store, "run")?;
     let ret = run.call_async(&mut store, ()).await?;
     println!("result of run:\n{}", ret.0);
 
+    Ok(())
 
-
-
-
-
-
-
-
-
-
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let listener = TcpListener::bind(addr).await?;
-
-    loop {
-        let (stream, _) = listener.accept().await?;
-
-        tokio::task::spawn(async move {
-            if let Err(err) = http1::Builder::new()
-                .serve_connection(stream, service_fn(echo))
-                .await
-            {
-                println!("Error serving connection: {:?}", err);
-            }
-        });
-    }
-}
-
-/// This is our service handler. It receives a Request, routes on its
-/// path, and returns a Future of a Response.
-async fn echo(
-    req: Request<hyper::body::Incoming>,
-) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
-    match (req.method(), req.uri().path()) {
-        // Serve some instructions at /
-        (&Method::GET, "/") => Ok(Response::new(full(
-            "Try POSTing data to /echo such as: `curl localhost:3000/echo -XPOST -d \"hello world\"`",
-        ))),
-
-        // Simply echo the body back to the client.
-        (&Method::POST, "/echo") => Ok(Response::new(req.into_body().boxed())),
-
-        // Convert to uppercase before sending back to client using a stream.
-        (&Method::POST, "/echo/uppercase") => {
-            let frame_stream = req.into_body().map_frame(|frame| {
-                let frame = if let Ok(data) = frame.into_data() {
-                    data.iter()
-                        .map(|byte| byte.to_ascii_uppercase())
-                        .collect::<Bytes>()
-                } else {
-                    Bytes::new()
-                };
-
-                Frame::data(frame)
-            });
-            Ok(Response::new(frame_stream.boxed()))
-        }
-
-        // Return the 404 Not Found for other routes.
-        _ => {
-            let mut not_found = Response::new(empty());
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            Ok(not_found)
-        }
-    }
-}
-
-fn empty() -> BoxBody<Bytes, hyper::Error> {
-    Empty::<Bytes>::new()
-        .map_err(|never| match never {})
-        .boxed()
-}
-
-fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
-    Full::new(chunk.into())
-        .map_err(|never| match never {})
-        .boxed()
 }
