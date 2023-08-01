c4a22fc5b7e75bd2b2585eeb4153a1ff2d2e0616
Bailey Hayes <behayes2@gmail.com>
2023-08-01 01:09:05 -0400

    fix: string concat in c

--- guest/c/component.c
+++ guest/c/component.c
@@ -5,9 +5,14 @@
 
 void exports_component_example_greeting_hello(critter_string_t *greeting, critter_string_t *ret) {
     critter_string_t a;
-    critter_string_set(&a, "\nGreetings from C! 🐱");
+    critter_string_set(&a, "\nGreetings from 🐱 (C)!");
+
+    ret->len = greeting->len + a.len;
+    ret->ptr = malloc(ret->len * 2);
 
     // append critter to greeting and return
-    strcpy(ret->ptr, a.ptr);
+    strcpy(ret->ptr, greeting->ptr);
+    strcat(ret->ptr, a.ptr);
     critter_string_free(&a);
+    critter_string_free(greeting);
 }
